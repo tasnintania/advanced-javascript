db987069474d2aae78cc60cf29f685c9780885e8
tasnintania <tasnin.tania441996@gmail.com>
2020-08-07 16:09:42 +0600

    undefiend  vs  null clear

---
+++ undefined-null.js
@@ -0,0 +1,20 @@
+let pakhi;
+function add(num1 , num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1,num2){
+    console.log(num1,num2);
+}
+
+const tania = {name:"vnias",phone: 456321789};
+
+console.log(tania.bf);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [2,5,6]
+console.log(ages[12]);
+
