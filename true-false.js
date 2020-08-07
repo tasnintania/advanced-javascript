6fd54ebd32fe54bd91cd66ad9762e0ae2dd434cf
tasnintania <tasnin.tania441996@gmail.com>
2020-08-07 15:35:00 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,19 @@
+//Falsy:
+// 0
+//""
+//undefined
+//null
+//nan
+
+
+//true:
+//"0", " ", [], {}
+
+const age = 4;
+if(age){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
+
